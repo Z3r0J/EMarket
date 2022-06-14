1677fbcbb5291c43e8ddf429a15258f57c4b0fed
Jean Carlos Reyes Encarnacion <yacalo45@hotmail.es>
2022-06-14 18:31:22 -0400

    Add project files.

---
+++ EMarket.Core.Application/ServicesRegistration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMarket.Core.Application
+{
+    internal class ServicesRegistration
+    {
+    }
+}
