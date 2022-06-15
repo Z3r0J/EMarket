ebc2404850e88700435f514d9e5884168232622d
Jean Carlos Reyes Encarnacion <yacalo45@hotmail.es>
2022-06-14 21:50:09 -0400

    Entities and Common Properties

---
+++ EMarket.Core.Domain/Entities/Advertising.cs
@@ -0,0 +1,20 @@
+﻿using EMarket.Core.Domain.Common;
+using System.Collections.Generic;
+
+namespace EMarket.Core.Domain.Entities
+{
+    public class Advertising : AuditableBaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Categories { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<Gallery> Gallery { get; set; }
+    }
+}
---
+++ EMarket.Core.Domain/Entities/Gallery.cs
@@ -0,0 +1,14 @@
+﻿using EMarket.Core.Domain.Common;
+
+namespace EMarket.Core.Domain.Entities
+{
+    public class Gallery : AuditableBaseEntity
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+        //Navigation Properties
+        public int AdvertisingId { get; set; }
+        public Advertising Advertisings { get; set; }
+    }
+}
